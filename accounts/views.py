9fd38e6f44370ac7626e05ceddf0cf5be032fb13
Piotr Nowakowski <nowakowski.peter@gmail.com>
2018-12-30 21:06:59 +0000

    Added login form and logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib import auth, messages
+from accounts.forms import UserLoginForm
 
 # Create your views here.
 
@@ -7,7 +8,21 @@ def acccounts_index(request):
     return render(request, 'index.html')
     
 def login(request):
-    return render(request, 'login.html')
+    form = UserLoginForm(request.POST if request.POST else None)
+    if request.method == "POST":
+        if form.is_valid():
+            # user will either be a User object or False
+            user = auth.authenticate(username = request.POST['username'], password = request.POST['password'])
+            if user:
+                messages.success(request, "You have successfully logged in!")
+                auth.login(user = user, request = request)
+            else:
+                #messages.error("The username or password is invalid")
+                form.add_error(None, "The username or password is invalid")
+        else:
+            form.add_error(None, "There was an error with form input")
+            
+    return render(request, 'login.html', {"form": form })
     
 def logout(request):
     if request.user.is_authenticated:
