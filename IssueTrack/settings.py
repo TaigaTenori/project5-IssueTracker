d68873f30826a582ed5392bde24f9a101c9639d5
Piotr Nowakowski <nowakowski.peter@gmail.com>
2019-01-02 23:41:10 +0000

    Moved base.html to top level templates/ directory since it will be used by all pages

--- IssueTrack/settings.py
+++ IssueTrack/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'IssueTrack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
