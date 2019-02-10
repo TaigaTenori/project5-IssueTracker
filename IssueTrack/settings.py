baa94b35332155419e35a61f38a1b04a49b76f68
Piotr Nowakowski <nowakowski.peter@gmail.com>
2019-02-10 15:55:29 +0000

    .

--- IssueTrack/settings.py
+++ IssueTrack/settings.py
@@ -136,8 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_DIR, "static"),
 ]
 
 STRIPE_PUBLISHABLE = os.getenv("STRIPE_PUBLISHABLE")
